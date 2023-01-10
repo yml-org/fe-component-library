08a6093130067751fe86efd89695d87008ff59cd
Ramyarnayak <ramya.nayak@ymedialabs.com>
2023-01-10 12:42:01 +0530

    changed headerText from Home to YML

--- packages/header/src/header/header.component.ts
+++ packages/header/src/header/header.component.ts
@@ -47,7 +47,7 @@ export class HeaderComponent extends TailwindElement(Style) {
     mode: 'light',
     logo: 'https://ymedialabs.atlassian.net/s/1jmxwi/b/8/d35727372e299c952e88a10ef82bbaf6/_/jira-logo-scaled.png',
     logoAltText: msg(str`Company logo`),
-    headerText: 'Home',
+    headerText: 'YML',
     menuIcon: '',
     menuLinks: [
       {
