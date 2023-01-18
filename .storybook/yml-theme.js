03b213ac5cb91b05063fc3a38c25e5ad7496b572
NileshSinhaYML <nilesh.sinha@ymedialabs.com>
2023-01-18 18:09:23 +0530

    feat(storybook): update logo in storybook

---
+++ .storybook/yml-theme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'YML',
+  brandUrl: 'https://yml.co/',
+  brandImage: 'https://avatars.githubusercontent.com/u/100911795?s=200&v=4',
+  brandTarget: '_self',
+});
