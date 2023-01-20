3151cc7c83a47f6ef522efffd34095f484741f29
ng22792yml <nirlipta.gaur@ymedialabs.com>
2023-01-20 09:54:33 +0530

    Added test cases for Header Component

--- packages/header/src/header/header.component.spec.ts
+++ packages/header/src/header/header.component.spec.ts
@@ -4,10 +4,9 @@ describe('header-component', () => {
   const HEADER_COMPONENT = 'header-component';
   let headerElement: LitElement;
 
-  // TODO: Uncomment when needed to access the shadow root
-  // const getShadowRoot = (tagName: string): ShadowRoot => {
-  //   return document.body.getElementsByTagName(tagName)[0].shadowRoot;
-  // };
+  const getShadowRoot = (tagName: string): ShadowRoot => {
+    return document.body.getElementsByTagName(tagName)[0].shadowRoot;
+  };
 
   beforeEach(() => {
     headerElement = window.document.createElement(
@@ -25,4 +24,60 @@ describe('header-component', () => {
       document.body.getElementsByTagName(HEADER_COMPONENT)[0]
     ).toMatchSnapshot();
   });
+
+  it('sets the openMenu to false', async () => {
+    headerElement.setAttribute('openMenu', 'false');
+    window.document.body.appendChild(headerElement);
+    await headerElement.updateComplete;
+    const renderedComponent = getShadowRoot(HEADER_COMPONENT).innerHTML;
+    expect(renderedComponent.indexOf('false')).not.toBe(-1);
+  });
+
+  it('sets the openMenu to true', async () => {
+    headerElement.setAttribute('openMenu', 'true');
+    window.document.body.appendChild(headerElement);
+    await headerElement.updateComplete;
+    const renderedComponent = getShadowRoot(HEADER_COMPONENT).innerHTML;
+    expect(renderedComponent.indexOf('true')).not.toBe(-1);
+  });
+
+  it('sets the default header hoverTextColor correctly', async () => {
+    const headerDivClassList =
+      document.body.getElementsByClassName('hover:text-white')[0];
+    expect(headerDivClassList).not.toBe(-1);
+  });
+
+  // Test Case Failing
+  it('sets the default header headerTextColor correctly', async () => {
+    const headerDivClassList =
+      document.body.getElementsByClassName('text-white');
+    console.log(headerDivClassList);
+    expect(headerDivClassList).toHaveLength(1);
+  });
+
+  it('sets the default header headerTextColor correctly', async () => {
+    const navOptions = {
+      mode: 'dark',
+      logo: 'https://ymedialabs.atlassian.net/s/1jmxwi/b/8/d35727372e299c952e88a10ef82bbaf6/_/jira-logo-scaled.png',
+      headerText: 'YML',
+      openMenuIcon: {
+        slotName: '',
+      },
+      closeMenuIcon: {
+        slotName: '',
+      },
+      menuLinks: [],
+      isMenuOpen: false,
+      topRightSlot: {
+        slotName: 'bell',
+      },
+    };
+
+    headerElement.setAttribute('navOptions', JSON.stringify(navOptions));
+    window.document.body.appendChild(headerElement);
+    await headerElement.updateComplete;
+    console.log(HEADER_COMPONENT);
+    // Fails to change anything in the above consoled HEADER_COMPONENT
+  });
+
 });
