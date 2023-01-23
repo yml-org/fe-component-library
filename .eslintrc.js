1a74d403773c80d777d0d178b9a5b1bc6c436628
Abdul Ziyan <abdul.ziyan@ymedialabs.com>
2023-01-23 10:22:52 +0530

    Resolved all requested changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,9 +9,7 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  rules: {
-    // "@typescript-eslint/no-inferrable-types": "off",
-  },
+  rules: {},
   plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
