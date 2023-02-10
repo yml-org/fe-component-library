6cf067480bc667ed5f12f03b0503351aa80f43e3
aswathisunilkumar <aswathi.s@ymedialabs.com>
2023-02-10 11:06:03 +0530

    Implement google analytics accelerator

--- packages/ga-accelerator/src/index.ts
+++ packages/ga-accelerator/src/index.ts
@@ -1,5 +1,3 @@
-import { uuid } from 'uuidv4';
-
 export const gaInstall = (trackingId, additionalConfig = {}) => {
   const scriptId = 'ga-accelerator';
 
@@ -30,31 +28,11 @@ export const gaCookieLessInstall = (trackingId) => {
   head.insertBefore(script, head.firstChild);
 
   window.dataLayer = window.dataLayer || [];
-  const GA_LOCAL_STORAGE_KEY = 'ga-clientId';
-
-  // checks if localstorage is available
-  if (window.localStorage) {
-    // checks if user was already connected and loads client_id from localstorage
-    if (localStorage.getItem(GA_LOCAL_STORAGE_KEY)) {
-      // creates new tracker with same client_id as the last time the user visited
-      gtag('js', new Date());
-      gtag('config', trackingId, {
-        send_page_view: true,
-        client_storage: 'none',
-        client_id: localStorage.getItem(GA_LOCAL_STORAGE_KEY),
-      });
-    } else {
-      // creates client_id and saves it in localStorage -> currently random number better would be a uuid
-      window.localStorage.setItem(GA_LOCAL_STORAGE_KEY, uuid());
-      // creates new tracker with the new client_id
-      gtag('js', new Date());
-      gtag('config', trackingId, {
-        send_page_view: true,
-        client_storage: 'none',
-        client_id: localStorage.getItem(GA_LOCAL_STORAGE_KEY),
-      });
-    }
-  }
+  gtag('js', new Date());
+  gtag('config', trackingId, {
+    anonymize_ip: true,
+    client_storage: 'none',
+  });
 };
 
 export const gaAccelerator = function (...args) {
