aa63835ec6db70f3296409233e0af9af7896a725
NsinhaYML <NSinha@teampurpose.com>
2023-03-13 20:22:20 +0530

    test

--- scripts/cem-plugin-react-wrapper.js
+++ scripts/cem-plugin-react-wrapper.js
@@ -1,129 +1,25 @@
+/* eslint-disable */
+
 import fs from 'fs';
 import path from 'path';
 import prettier from 'prettier';
 
+import {
+  camelize,
+  createEventName,
+  has,
+  getDefineCallForElement,
+  RESERVED_WORDS,
+} from './utils.js';
+
 const packageJsonPath = `${process.cwd()}${path.sep}package.json`;
 const packageJson = JSON.parse(fs.readFileSync(packageJsonPath).toString());
-
-function getDefineCallForElement(cem, tagName) {
-  let result = undefined;
-
-  cem?.modules?.forEach((_module) => {
-    _module?.exports?.forEach((ex) => {
-      if (ex.kind === 'custom-element-definition' && ex.name === tagName)
-        result = _module.path;
-    });
-  });
-
-  return result;
-}
-
-function camelize(str) {
-  const arr = str.split('-');
-  const capital = arr.map((item, index) =>
-    index
-      ? item.charAt(0).toUpperCase() + item.slice(1).toLowerCase()
-      : item.toLowerCase()
-  );
-  return capital.join('');
-}
-
-function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-const has = (arr) => Array.isArray(arr) && arr.length > 0;
-
-const RESERVED_WORDS = [
-  'children',
-  'localName',
-  'ref',
-  'style',
-  'className',
-  'abstract',
-  'arguments',
-  'await',
-  'boolean',
-  'break',
-  'byte',
-  'case',
-  'catch',
-  'char',
-  'class',
-  'const',
-  'continue',
-  'debugger',
-  'default',
-  'delete',
-  'do',
-  'double',
-  'else',
-  'enum',
-  'eval',
-  'export',
-  'extends',
-  'false',
-  'final',
-  'finally',
-  'float',
-  'for',
-  'function',
-  'goto',
-  'if',
-  'implements',
-  'import',
-  'in',
-  'instanceof',
-  'int',
-  'interface',
-  'let',
-  'long',
-  'native',
-  'new',
-  'null',
-  'package',
-  'private',
-  'protected',
-  'public',
-  'return',
-  'short',
-  'static',
-  'super',
-  'switch',
-  'synchronized',
-  'this',
-  'throw',
-  'throws',
-  'transient',
-  'true',
-  'try',
-  'typeof',
-  'var',
-  'void',
-  'volatile',
-  'while',
-  'with',
-  'yield',
-];
-
-/**
- * ATTRIBUTE/PROPERTY NAME CLASHES:
- * It could be the case that an attr/property are not correctly linked together (e.g.: the attr does not have a `fieldName` pointing
- * to the property). In that case, there will be two props passed to the React component function with the same name, which will break things
- * Make sure to document components correctly (in most cases, all you have to do is add an @attr jsdoc to the field)
- *
- * Attrs and properties are distinguished by an attr's `fieldName`. If an attr has a `fieldName`, we ignore it as being an attribute, and
- * only use the property (which is whatever the `fieldName` points to). If an attr does not have a `fieldName`, we apply it as an attr
- *
- * EVENTS:
- * `'selected-changed'` event expects a function passed as `onSelectedChanged` (we add the 'on', and we camelize and capitalize the event name)
- */
 
 export default function reactify({
   exclude = [],
   attributeMapping = {},
   outdir = 'legacy',
-}) {
+} = {}) {
   return {
     name: 'reactify',
     packageLinkPhase({ customElementsManifest }) {
@@ -189,7 +85,7 @@ export default function reactify({
 
         let params = [];
         component?.events?.forEach((event) => {
-          params.push(`on${capitalizeFirstLetter(camelize(event.name))}`);
+          params.push(createEventName(event));
         });
 
         fields?.forEach((member) => {
@@ -204,9 +100,6 @@ export default function reactify({
 
         params = params?.join(', ');
 
-        const createEventName = (event) =>
-          `on${capitalizeFirstLetter(camelize(event.name))}`;
-
         const events = component?.events?.map(
           (event) => `
             useEffect(() => {
@@ -216,7 +109,7 @@ export default function reactify({
           )});
               }
             }, [])
-`
+          `
         );
 
         const booleanAttrs = booleanAttributes?.map(
@@ -230,7 +123,7 @@ export default function reactify({
                 }
               }
             }, [${attr?.originalName ?? attr.name}])
-`
+          `
         );
 
         const attrs = attributes?.map(
